311d86b4a60324ac80f0db8600153c3351e6e674
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-02-11 16:44:00 +0900

    add transform markdown to html

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+// コマンドライン引数をパースするモジュール
 const program = require("commander");
 
 // ファイルの入出力をするモジュール
 const fs = require("fs");
 
+// MarkdownをHTMLへ変換するモジュール
+const marked = require("marked");
+
 //コマンドライン引数をパースする
+program.option("--gfm", "enable gfm");
 program.parse(process.argv);
 
 //ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-// console.log(filePath);
 
+// デフォルトのコマンドライン引数を設定、 ユーザー入力で上書きする
+const cliOptions = {
+  gfm: false,
+  ...program.opts()
+};
 // ファイルを非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
   if (err) {
@@ -17,5 +26,8 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+  const html = marked(file, {
+    gfm: cliOptions.gfm
+  });
+  console.log(html);
 });
