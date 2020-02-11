0d8b09b1784e018ad80cfab844fa608406deaf2c
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-02-11 16:24:07 +0900

    add read file function

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const program = require("commander");
 
+// ファイルの入出力をするモジュール
+const fs = require("fs");
+
 //コマンドライン引数をパースする
 program.parse(process.argv);
 
 //ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+// console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
